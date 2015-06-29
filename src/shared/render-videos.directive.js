6b808f9445d3add6d08c56487d3e886e71ae5e6b
Bruno Paixão <brunomrpx@gmail.com>
2015-06-29 01:34:48 -0300

    Improved video details presentation

--- src/shared/render-videos.directive.js
+++ src/shared/render-videos.directive.js
@@ -5,13 +5,47 @@
         .module('Application')
         .directive('renderVideos', RenderVideos);
 
-    function RenderVideos() {
-        return {
+    RenderVideos.$inject = ['$timeout', '$templateCache', '$interpolate'];
+
+    function RenderVideos($timeout, $templateCache, $interpolate) {
+        var directiveDefinition = {
             restrict: 'E',
             templateUrl: 'src/shared/render-videos.directive.html',
             scope: {
                 videos: '='
-            }
+            },
+            link: link
         };
+
+        return directiveDefinition;
+
+        function link(scope, element, attributes) {
+            scope.$watch('videos', function(newVideos, oldVideos)  {
+                $timeout(function() {
+                    if (scope.videos) {
+                        console.log('videos');
+                        var id;
+                        var elementQuery;
+                        var video;
+                        var content;
+                        var popoverTemplate = $interpolate($templateCache.get('popoverContent.html'));
+                        for (var index in newVideos) {
+                            video = newVideos[index].video;
+                            elementQuery = ".video-" + video.video_id;
+                            var content = popoverTemplate({
+                                video: video
+                            });
+                            $(elementQuery).popover({
+                                trigger: 'hover',
+                                title: video.title,
+                                content: content,
+                                placement: 'bottom',
+                                html: true
+                            });
+                        }
+                    }
+                });
+            });
+        }
     }
 })();
